a3337f2c16be14c667420d36823702faaa4891f4
jiyouon <141696842+jiyouon@users.noreply.github.com>
2025-02-18 22:18:18 +0900

    서버 메인 파일
    기존 HTML 형식 -> JSON
    코드 분할 및 중복 삭제 완

--- Backend/app.py
+++ Backend/app.py
@@ -1,39 +1,31 @@
-from flask import Flask, request, redirect, url_for, session, flash
+from flask import Flask
 from flask_mysqldb import MySQL
-from flask_cors import CORS
+from flask_cors import CORS  # React와 연결
+from auth import auth_bp
+from profile import profile_bp
+from rental import rental_bp
+from rental_return import return_bp 
 
-# Flask 애플리케이션 초기화
 app = Flask(__name__)
-CORS(app)
 
-# 세션 키 설정
-app.secret_key = 'test'
+# React와 연결 가능하게 함 프론트에서 확인!
+CORS(app, supports_credentials=True)
 
 # MySQL 설정
+app.secret_key = 'test'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'test'  -> 실제 비밀번호
-app.config['MYSQL_DB'] = 'student24_db'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'test'
 app.config['MYSQL_CHARSET'] = 'utf8mb4'
 
 mysql = MySQL(app)
 
-# Blueprint 라우트 등록
-from routes.auth_routes import auth_bp
-from routes.rental_routes import rental_bp
-from routes.return_routes import return_bp
-from routes.admin_routes import admin_bp
+# 블루프린트 등록
+app.register_blueprint(auth_bp)
+app.register_blueprint(profile_bp)
+app.register_blueprint(rental_bp)
+app.register_blueprint(return_bp)
 
-app.register_blueprint(auth_bp, url_prefix='/api/auth')
-app.register_blueprint(rental_bp, url_prefix='/api/rental')
-app.register_blueprint(return_bp, url_prefix='/api/return')
-app.register_blueprint(admin_bp, url_prefix='/api/admin')
-
-# 메인 페이지 리다이렉트 (index → 로그인 페이지)
-@app.route('/')
-def index():
-    return redirect(url_for('auth.login'))
-
-# Flask 실행
 if __name__ == '__main__':
     app.run(debug=True)
