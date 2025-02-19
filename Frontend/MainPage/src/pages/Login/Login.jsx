22c520b17b9f56c43b61d646ec5f4ccd1a48f6d9
OrdinaryYoni <lgayeon07@gmail.com>
2025-02-20 00:18:40 +0900

    로그인 아마 끝

--- Frontend/MainPage/src/pages/Login/Login.jsx
+++ Frontend/MainPage/src/pages/Login/Login.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom"; // useNavigate 추가
 function Login() {
   let [id, setId] = useState("");
   let [pw, setPw] = useState("");
+  const [showPswd, setShowPswd] = useState(false);
   let navigate = useNavigate(); // 네비게이션 훅 사용
 
   const handleSubmit = (e) => {
@@ -18,12 +19,17 @@ function Login() {
     navigate("/", { state: { id, pw } });
   };
 
+  const ShowPwCheck = () => {
+    setShowPswd((prev) => !prev); // 이전 값의 반대로 변경
+  };
+
   return (
     <div className="Login">
       <div className="container">
         <h1>로그인</h1>
+
         <form onSubmit={handleSubmit}>
-          <label htmlFor="studentId">학번:</label>
+          <label className="studentId">아이디:</label>
           <input
             type="text"
             id="studentId"
@@ -33,9 +39,9 @@ function Login() {
             onChange={(e) => setId(e.target.value)}
             required
           />
-          <label htmlFor="password">비밀번호:</label>
+          <label className="password">비밀번호:</label>
           <input
-            type="password"
+            type={showPswd ? "text" : "password"}
             id="password"
             name="password"
             placeholder="비밀번호를 입력해 주세요"
@@ -43,7 +49,14 @@ function Login() {
             onChange={(e) => setPw(e.target.value)}
             required
           />
+          <label>
+            <input type="checkbox" onChange={ShowPwCheck} />
+            <span>비밀번호 보기</span>
+          </label>
           <button type="submit">로그인</button>
+          <p className="info-text">
+            기본 비밀번호는 마이페이지에서 변경할 수 있어요!
+          </p>
         </form>
       </div>
     </div>
